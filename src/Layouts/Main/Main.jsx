41fb21063193a3ae2b17636e6670cbd4dc841229
Pintu-Roy121 <roypintu121@gmail.com>
2023-07-15 20:52:34 +0600

    first commit

---
+++ src/Layouts/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Pages/Navbar/Navbar';
+import Home from '../../Pages/Home/Home';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Main;
